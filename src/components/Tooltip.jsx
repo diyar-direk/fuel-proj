6e4e16f473eec70741c1c2e8223e3e98f9e36cc4
raed-bash <raed09321@gmail.com>
2025-02-15 19:11:39 +0300

    Develop Tooltip component

---
+++ src/components/Tooltip.jsx
@@ -0,0 +1,50 @@
+import { memo, useState } from "react";
+/**
+ * @typedef placement
+ * @type {"top"|"left"|"right"|"bottom"}
+ */
+
+/**
+ * @typedef utils
+ * @property {string} title
+ * @property {placement} placement
+ */
+
+/**
+ * @typedef tooltipProps
+ * @type {React.HTMLAttributes<HTMLDivElement> & utils}
+ */
+
+/**
+ * @param {tooltipProps} props
+ */
+function Tooltip({ children, title, placement = "bottom", ...props }) {
+  const [mouseIn, setMouseIn] = useState(false);
+
+  return (
+    <div
+      {...props}
+      className="relative"
+      onMouseEnter={() => setMouseIn(true)}
+      onMouseLeave={() => setMouseIn(false)}
+    >
+      {children}
+      {mouseIn && (
+        <div
+          className={`absolute p-2 text-sm bg-dark-light text-white rounded font-medium ${placementStyle[placement]}`}
+        >
+          {title}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default memo(Tooltip);
+
+const placementStyle = {
+  bottom: "-bottom-11 left-1/2 -translate-x-1/2",
+  top: "-top-11 left-1/2 -translate-x-1/2",
+  left: "-left-20 top-1/2 -translate-y-1/2",
+  right: "-right-20 top-1/2 -translate-y-1/2",
+};
