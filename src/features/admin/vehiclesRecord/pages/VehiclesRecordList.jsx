53b7c2a582fb0670a84ebf1979e6f9fd537fecae
raed-bash <raed09321@gmail.com>
2025-02-20 22:50:52 +0300

    Add Path controll feature to layout

--- src/features/admin/vehiclesRecord/pages/VehiclesRecordList.jsx
+++ src/features/admin/vehiclesRecord/pages/VehiclesRecordList.jsx
@@ -1,32 +1,24 @@
-import { VehicleIcon } from "../../../../assets/icons";
-import PagePath from "../../../../components/PagePath";
 import VehiclesRecordGeneralSchema from "../components/VehiclesRecordGeneralSchema";
 import VehiclesRecordTable from "../components/VehiclesRecordTable";
 import VehiclesRecordToolBar from "../components/VehiclesRecordToolBar";
+import useAddPath from "../../../../hooks/useAddPath";
+
+const paths = [{ children: "آليات" }, { children: "محطات" }];
 
 function VehiclesRecordList() {
+  useAddPath(paths);
+
   return (
     <div>
-      <PagePath
-        paths={[
-          {
-            children: <VehicleIcon className="fill-primary-main" />,
-          },
-          { children: "آليات" },
-          { children: "محطات" },
-        ]}
-      />
-      <div>
-        <div className="flex justify-between items-center px-16 max-md:px-9 max-sm:px-5 py-3">
-          <p className="text-primary-main text-3xl max-md:text-xl max-sm:text-base font-medium">
-            استعراض سجل الآليات
-          </p>
-          <VehiclesRecordToolBar />
-        </div>
-        <div className="flex gap-2 bg-secondary-main">
-          <VehiclesRecordGeneralSchema />
-          <VehiclesRecordTable />
-        </div>
+      <div className="flex justify-between items-center px-16 max-md:px-9 max-sm:px-5 py-3">
+        <p className="text-primary-main text-2xl max-md:text-xl max-sm:text-base font-medium">
+          استعراض سجل الآليات
+        </p>
+        <VehiclesRecordToolBar />
+      </div>
+      <div className="flex gap-2 bg-secondary-main">
+        <VehiclesRecordGeneralSchema />
+        <VehiclesRecordTable />
       </div>
     </div>
   );
