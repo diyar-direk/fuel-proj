acbc043ee489efb324dc159ce6185f9eecb2e905
raed-bash <raed09321@gmail.com>
2025-03-06 17:23:16 +0300

    Add handleErrNetwork to axios interceptor

--- src/hooks/useAxiosInterceptor.js
+++ src/hooks/useAxiosInterceptor.js
@@ -12,17 +12,33 @@ export default function useAxiosInterceptor() {
     }
   }, []);
 
+  const handleErrNetwork = useCallback((err) => {
+    if (err.message) {
+      const message = err.message;
+
+      toast.error(message);
+    }
+  }, []);
+
   useLayoutEffect(() => {
-    axiosInstance.interceptors.response.use(
+    const id = axiosInstance.interceptors.response.use(
       (res) => {
         handleResponse(res);
         return res;
       },
       (err) => {
-        handleResponse(err.response);
+        if (err.response) {
+          handleResponse(err.response);
+        }
+
+        if (err.code === "ERR_NETWORK") {
+          handleErrNetwork(err);
+        }
 
         return Promise.reject(err);
       }
     );
-  }, [handleResponse]);
+
+    return () => axiosInstance.interceptors.response.eject(id);
+  }, [handleResponse, handleErrNetwork]);
 }
